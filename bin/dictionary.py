40dc9e5c411960ff0a47411d5ef5d6037ecc702d
Katie Silverio <astrosilverio@gmail.com>
2013-10-16 15:57:06 -0400

    Changed the Neville-toad quest a bit

--- bin/dictionary.py
+++ bin/dictionary.py
@@ -1,4 +1,4 @@
 directions = ['n', 's', 'e', 'w', 'nw', 'ne', 'sw', 'se', 'u', 'd']
-nouns = ['bludger', 'snitch', 'owl', 'cat', 'broom', 'spoon', 'wand', 'food', 'potion', 'toilet', 'diary', 'tea', 'mirror', 'raccoon', 'snitch', 'statue', 'willow', 'door', 'candy', 'bones', 'scrap of paper', 'trevor', 'cloak']
-people = ['hagrid', 'mcgonagall', 'dumbledore', 'snape', 'nick', 'myrtle', 'nightwing', 'crookshanks', 'bellatrix', 'flitwick', 'pince', 'fang', 'mermaid', 'neville']
+nouns = ['bludger', 'snitch', 'cat', 'broom', 'spoon', 'wand', 'food', 'potion', 'toilet', 'diary', 'tea', 'mirror', 'raccoon', 'snitch', 'statue', 'willow', 'door', 'candy', 'bones', 'scrap of paper', 'trevor', 'cloak']
+people = ['hagrid', 'mcgonagall', 'dumbledore', 'snape', 'nick', 'myrtle', 'nightwing', 'crookshanks', 'bellatrix', 'flitwick', 'pince', 'fang', 'mermaid', 'neville', 'owl']
 spells = ['lumos', 'riddikulus', 'patronum', 'witch', 'alohomora', 'nox', 'wingardium']
--- people.py
+++ people.py
@@ -16,16 +16,16 @@ class Person(object):
 		if self.quest_item:
 			if self.quest_item in player.invent:
 				player.remove_invent(self.quest_item)
-				print "%s says %s" % (self.name, self.quest_complete)
+				print self.quest_complete
 				print self.reward[0] + '\n'
 				room_object.add_invent(self.reward[1])
 				del self.dialogue[0]
 		if self.count < len(self.dialogue):
-			print "%s says %s" % (self.name, self.dialogue[self.count])
+			print self.dialogue[self.count]
 			self.count += 1
 		else:
 			self.count = 0
-			print "%s says %s" % (self.name, self.dialogue[self.count])
+			print self.dialogue[self.count]
 			self.count += 1
 
 
@@ -33,13 +33,15 @@ def make_people():
 
 	myrtle = Person("Myrtle", "Myrtle is amusing herself by sobbing in between dives into the U-bend.")
 	mermaid = Person("A Mermaid", "Behind the glass is a mermaid, whose teeth are displayed in a fearsome grin.")
-	neville = Person("Neville", "Neville Longbottom is pacing around.", quest_item='trevor', quest_complete="'Thanks! I don't know how he keeps getting away...'", reward=("In his excitement over reuniting with Trevor, Neville has dropped a small piece of paper.", 'scrap of paper'))
+	neville = Person("Neville", "Neville Longbottom is pacing around.", quest_item='trevor', quest_complete="Neville says, 'Thanks! I don't know how he keeps getting away...'", reward=("In his excitement over reuniting with Trevor, Neville has dropped a small piece of paper.", 'scrap of paper'))
+	owl = Person("Owl", "A large owl blinks at you intelligently.", quest_item='food', quest_complete="The owl wolfs down the food you brought him, clicking his beak.", reward=("The owl squawks 'TREVOR!' and scuffs away some droppings to reveal a small-ish toad.", 'trevor'))
 	
-	myrtle.add_dialogue(["'You're just here to throw a book at me!'", "'Leave me alone!', and dives back into the toilet."])
-	mermaid.add_dialogue(["something unintelligible that sounds like nails on a chalkboard.", "something that is clearly not friendly.", "-- why are you still here? She obviously wants to eat you, leave her alone!"])
-	neville.add_dialogue(["'I seem to have misplaced my toad. Can you find him?'", "'The Room of Requirement is stuck in sauna form, so the DA's meeting in the dungeons later.'", "'Have we met? I'm Neville.'"])
+	myrtle.add_dialogue(["Myrtle says, 'You're just here to throw a book at me!'", "Myrtle says, 'Leave me alone!', and dives back into the toilet."])
+	mermaid.add_dialogue(["The mermaid says something unintelligible that sounds like nails on a chalkboard.", "The mermaid says something that is clearly not friendly.", "The merm--hang on, why are you still here? She obviously wants to eat you, leave her alone!"])
+	neville.add_dialogue(["Neville says, 'I seem to have misplaced my toad. Can you find him?'", "Neville says, 'The Room of Requirement is stuck in sauna form, so the DA's meeting in the dungeons later.'", "Neville says,'Have we met? I'm Neville.'"])
+	owl.add_dialogue(["The owl squawks piteously, taps his stomach with one wing, and clicks his beak as if chewing.", "You feel a bit silly talking to an owl. He chirps in sympathy for your unease."])
 		
-	npc = {'myrtle': myrtle, 'mermaid': mermaid, 'neville': neville}
+	npc = {'myrtle': myrtle, 'mermaid': mermaid, 'neville': neville, 'owl': owl}
 	return npc
 
 npc = make_people()
